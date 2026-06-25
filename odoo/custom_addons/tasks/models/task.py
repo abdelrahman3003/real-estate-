f0e15ac349c4a51fc560eac2caa15219c742af0e
hello <abdelrahmantemsah30@gmail.com>
2026-06-25 14:56:33 +0300

    add cron feature

--- odoo/custom_addons/tasks/models/task.py
+++ odoo/custom_addons/tasks/models/task.py
@@ -12,16 +12,34 @@ class Task(models.Model):
         ('new', 'New' ),
         ('in_progress', 'In Progress'),
         ('completed', 'Completed'),
+        ('closed', 'Closed'),
     ],default='new')
     create_date = fields.Datetime(readonly=True)  # built-in field
     date_only = fields.Date(string="Creation Date", compute="_compute_date_only", store=False)
     assign_to = fields.Many2one("res.partner")
     active=fields.Boolean(default=True)
+    deadline = fields.Date()
+    is_late = fields.Boolean(default=False)
 
     @api.depends('create_date')
     def _compute_date_only(self):
         for rec in self:
             rec.date_only = rec.create_date.date() if rec.create_date else False
+
+    def action_closed(self):
+        for rec in self:
+            rec.status = 'closed'
+
+    def check_task_deadline_date(self):
+        task_ids = self.search([])
+        for rec in task_ids:
+            if rec.deadline and rec.deadline < fields.Date.today():
+                rec.is_late = True
+
+
+
+
+
 
 
 
